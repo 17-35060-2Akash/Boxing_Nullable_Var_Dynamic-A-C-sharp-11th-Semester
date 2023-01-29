33d391fd5d62e82e7dae86c0549fc18fbd9559d0
Akash <na.akash715@gmail.com>
2023-01-29 17:07:39 +0600

    first commit

---
+++ Boxing_Nullable_Var_Dynamic[A]/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxing_Nullable_Var_Dynamic_A_
+{
+    class Sample
+    {
+        //public int? y = 200;//nullable
+        //var y = "hello";//error
+        dynamic y;
+
+        //Error
+        //public void SampleMethod(var y = 4)
+        //{
+        //    var i = 5;
+        //}
+
+        public void SampleMethod(dynamic y)
+        {
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Boxing
+            //int x = 10;
+            //Object obj = x;//boxing,value will be known as boxed value
+            //x = 100;
+            //obj = 200;
+            //Console.WriteLine("x:"+x);
+            //Console.WriteLine("obj:" + obj);
+            //Unboxing
+            //int y =(int) obj;
+            //obj = 50;
+            //y = 100;
+            //Console.WriteLine("y:"+y);
+            //Console.WriteLine("obj:" + obj);
+
+            //Nullable
+            //Nullable<bool> check=null;
+            //check = true;
+            //Console.WriteLine(check.HasValue);
+            //Console.WriteLine("check:"+check);
+            //Nullable<int> x = null;
+            //int? x = null;
+            //x = 2;
+            //Console.WriteLine(x.HasValue);
+            //Console.WriteLine("x:" + x);
+            //Sample s = new Sample();
+            //s.y = null;
+
+            //var keyword
+            //var x=2;
+            //x = "hello";//error
+            //Console.WriteLine(x);
+
+            //dynamic keyword
+            dynamic x = 7;
+            Console.WriteLine(x.GetType());
+            x = "hello";
+            Console.WriteLine(x.GetType());
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ Boxing_Nullable_Var_Dynamic[A]/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Boxing_Nullable_Var_Dynamic[A]")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Boxing_Nullable_Var_Dynamic[A]")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7ee7aaf2-5e10-4cbd-81e0-17042a831cc1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
